149ff8729178f50187574e3f1c53e144c02d60f7
alex-nishikawa <alex.d.nishikawa@gmail.com>
2014-12-06 09:58:53 -0800

    Remove non-working method to stop highlighting when double clicking

--- learning_interface/js/Interface.js
+++ learning_interface/js/Interface.js
@@ -1,18 +1,4 @@
 $(function () {
-
-    $("#run").click(function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-      
-
-        elem.toggleClass('focused', elem.is(':visible'))
-            .toggle(elem.is(':visible'))
-
-        document.onselectstart = function () { return false; };
-        event.target.ondragstart = function () { return false; };
-        return false;
-    });
-
     $('#run').click(function () {
         var codeToRun = $('#editor').val();
         try {
