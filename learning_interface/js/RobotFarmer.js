4e086b8f1bc7babb5064f1efa8c7461c08842e00
alex-nishikawa <alex.d.nishikawa@gmail.com>
2014-12-06 09:17:01 -0800

    Interface: sends messages to console-output window

--- learning_interface/js/RobotFarmer.js
+++ learning_interface/js/RobotFarmer.js
@@ -1,17 +1,48 @@
-function CreateRobotFarmer() {
-    function LightsOn() {
-        //stub
+var robotFarmer = (function () {
+
+    var created = false,
+        serverUrl = "localhost:9000",
+        $console = $('#console-output');
+
+    var status = {
+        lightsOn: false,
+        waterOn: false
+    }
+    
+    function ConsoleMessage(message, type) {
+        if (type === 'sent') {
+            $console.append('Message Sent: ' + message + ' @' + GetFormattedCurrentTime() + '</br>');
+        }
+        else {
+            $console.append(message + ' @' + GetFormattedCurrentTime() + '</br>');
+        }
     }
 
-    function LightsOff() {
-        //stub
+    function GetFormattedCurrentTime() {
+        var time = new Date();
+        return time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
     }
 
-    function WaterOn() {
-        //stub
+    function PostMessage(messageToSend, displayMessage) {
+        $.ajax({
+            type: 'POST',
+            url: serverUrl,
+            data: messageToSend,
+            success: function() { $console.append(displayMessage, 'sent'); },
+            error: function(jqXHR,textStatus,errorThrown) { $console.append(errorThrown); }
+        })
     }
 
-    function WaterOff() {
-        //stub
+    function CreateRobotFarmer() {
+
+        ConsoleMessage("Robot Farmer Created!");
+        return {
+            lightsOn: PostMessage("{sender: 'student', message: 'lightsOn'}", 'lights on'),
+            status: function () { alert(status.lightsOn); }
+        };
     }
-}
+
+    return {
+        create: CreateRobotFarmer
+    };
+})();
