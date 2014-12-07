917a3ecd924873497e85fe75b3ef2832e84b23be
alexunwin <alex.unwin@infospace.com>
2014-12-07 15:01:47 -0800

    add color detection message detecting to cloud server

--- cloud_node_scripts/server-start.js
+++ cloud_node_scripts/server-start.js
@@ -1,12 +1,15 @@
+var image = require('./color_detection');
 var http = require('http');
 var fs = require('fs');
 var request = require('request');
 var qs = require('querystring');
 
-var raspPiEndpoint = "localhost:4000";
+var raspPiEndpoint = "192.168.1.3:4000";
+var selectedColor;
 
 var server = http.createServer(function(req, res) {
-	var msg = "";
+	var command = '';
+	var post;
 
 	if(req.method == 'POST') {
 		var body = ' ';
@@ -15,19 +18,52 @@ var server = http.createServer(function(req, res) {
 			console.log("BODY: " + body);
 		});
 		req.on('end', function() {
-			var post = JSON.parse(body);
-			msg = post.msg;
-			console.log("POST MESSAGE: " + post.msg);
+			post = JSON.parse(body);
+			command = post.cmd.toLowerCase();
+			console.log("COMMAND: " + command);
+
+			if(command == 'setcolor') {
+				selectedColor = post.data.color.toLowerCase();
+				
+				if(selectedColor.startsWith('#')) {
+					selectedColor = selectedColor.substring(1);
+				}
+
+				console.log("COLOR SET TO: " + selectedColor);
+			}
+
+			if(command == 'picturetaken') {
+				/*downloadImage('http://192.168.1.3:8080/arm_image.jpg', 'pic_from_pi.jpg', function() {
+					console.log('done');
+				});*/
+				console.log("pocessing image now...");
+				image.processImage('strawberry_initial.jpg', selectedColor);
+				//TODO: process picture, generate instructions and send back
+			}
+
+			if(command == 'lightsOn') {
+				var jsonBody = { 
+					'id' : 'farmerserver',
+					'cmd' : 'lightson',
+					'data' : {}
+				};
+
+				PostMessage(jsonBody, raspPiEndpoint);
+			}
+
+			if(command == 'lightsOff') {
+				var jsonBody = { 
+					'id' : 'farmerserver',
+					'cmd' : 'lightsoff',
+					'data' : {}
+				};
+
+				PostMessage(jsonBody, raspPiEndpoint);
+			}
 		});
 	}
 
-	if(msg == 'pictureTaken') {
-		downloadImage('http://192.168.1.3:8080/arm_image.jpg', 'pic_from_pi.jpg', function() {
-			console.log('done');
-		});
-
-		//TODO: process picture, generate instructions and send back
-	}
+	res.end("message recieved");
 	
 });
 
@@ -43,14 +79,20 @@ var downloadImage = function(url, filename, callback) {
 	});
 }
 
-function PostMessage(message, endpoint) {
+function PostMessage(jsonBody, endpoint) {
 	request.post(
 			endpoint,
-			{ json: {msg: message}},
+			{ json: jsonBody },
 			function (error, response, body) {
 				if(!error && response.statusCode == 200) {
 					console.log(body);
 				}
 			}
 		);
+}
+
+if(typeof String.prototype.startsWith != 'function') {
+	String.prototype.startsWith = function(str) {
+		return this.indexOf(str) == 0;
+	};
 }
