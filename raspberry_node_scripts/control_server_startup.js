641660a399eebaf100c0607605e05cd24cc5c3a2
alexunwin <alex.unwin@infospace.com>
2014-12-07 15:02:32 -0800

    add message protocol to pi server

--- raspberry_node_scripts/control_server_startup.js
+++ raspberry_node_scripts/control_server_startup.js
@@ -3,15 +3,43 @@ var request = require('request');
 var pythonShell = require('python-shell');
 
 var server = http.createServer(function(req, res) {
-	var message = 'takepicture';
 	
-	if(message === 'takepicture') {
-		TakePicture();
-		PostMessage('pictureTaken', 'http://192.168.1.2:3000');
-		console.log('POST: picture taken');
-	}
+	var command = '';
+	var post;
 
-	res.end('NodeJs running on Raspberry Pi');
+	if(req.method == 'POST') {
+		var body = ' ';
+		req.on('data', function(data) {
+			body += data;
+			console.log("BODY: " + body);
+		});
+		req.on('end', function() {
+			post = JSON.parse(body);
+			command = post.cmd.toLowerCase();
+			console.log("COMMAND: " + command);
+
+			if(command == 'takepicture') {
+				TakePicture();
+				var jsonBody = {
+					'id' : 'rasppi',
+					'cmd' : 'picturetaken',
+					'data' : {}
+				};
+				PostMessage(jsonBody, 'http://192.168.1.2:3000');
+				console.log('POST: picture taken');
+			}
+
+			if(command == 'lightson') {
+				//TODO; turn lights on
+			}
+
+			if(command == 'lightsoff') {
+				//TODO; turn lights off
+			}
+
+		});
+
+	res.end('message recieved');
 	console.log("Server visited");
 });
 
@@ -25,10 +53,10 @@ function TakePicture() {
 	});
 }
 
-function PostMessage(message, endpoint) {
+function PostMessage(jsonBody, endpoint) {
 	request.post(
 			endpoint,
-			{ json: {msg: message}},
+			{ json: jsonBody },
 			function (error, response, body) {
 				if(!error && response.statusCode == 200) {
 					console.log(body);
