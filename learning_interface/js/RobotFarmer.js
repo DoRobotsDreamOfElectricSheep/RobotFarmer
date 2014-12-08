b8cd107be2ccb1b463e2b370bc64c6904455fb6a
alex-nishikawa <alex.d.nishikawa@gmail.com>
2014-12-07 17:28:57 -0800

    Interface: poll for picture timestamp

--- learning_interface/js/RobotFarmer.js
+++ learning_interface/js/RobotFarmer.js
@@ -1,11 +1,12 @@
 var robotFarmer = (function () {
 
-    var serverUrl = "http://localhost:9000",
+    var serverUrl = "http://192.168.1.2:3000",
         $console = $('#console-output');
 
     var status = {
         lightsOn: false,
-        waterOn: false
+        waterOn: false,
+        pictureTimeStamp: 0
     }
     
     function ConsoleMessage(message, type) {
@@ -32,34 +33,68 @@ var robotFarmer = (function () {
 
     function PostMessage(messageToSend, displayMessage) {
         $.ajax({
-            type: 'GET',
+            type: 'POST',
+            url: serverUrl,
+            data: JSON.stringify(messageToSend),
+            success: function () { console.log(displayMessage, 'sent'); },
+            error: HandleAjaxError
+        })
+    }
+
+    function GetMessage(messageToSend, sucessCallback, displayMessage) {
+        $.ajax({
+            type: 'POST',
             url: serverUrl,
             contentType: "application/json; charset=utf-8",
-            data: messageToSend,
+            data: JSON.stringify(messageToSend),
             dataType: 'json',
-            success: function () { console.log(displayMessage, 'sent'); },
+            success: function (data) {
+                console.log(displayMessage, 'sent');
+                sucessCallback(data);
+            },
             error: HandleAjaxError
         })
     }
 
     function CreateRobotFarmer() {
         ConsoleMessage("Robot Farmer Created!");
+        PollForImageTimeStamp();
+
         return {
             lightsOn: TurnLightsOn,
             lightsOff: TurnLightsOff,
+            takePicture: TakeArmPicture,
             status: function () { console.log(status.lightsOn); }
         };
     }
 
     function TurnLightsOn(pos, timer) {
-        PostMessage("{id: 'student', cmd: 'lightsOn', data: { pos:" + pos + " , timer:" + timer + "} }", 'lights on');
+        PostMessage({id: 'student', cmd: 'lightsOn', data: { pos:pos , timer: timer } }, 'lights on');
     }
 
     function TurnLightsOff(pos, timer) {
-        PostMessage("{id: 'student', cmd: 'lightsOff', data: { pos:" + pos + " , timer:" + timer + "} }", 'lights off');
+        PostMessage({ id: 'student', cmd: 'lightsOff', data: { pos: pos, timer: timer } }, 'lights off');
     }
 
+    function TakeArmPicture() {
+        PostMessage({ id: 'student', cmd: 'takepicture'}, 'picture taken');
+    }
+
+    function PollForImageTimeStamp() {
+        setTimeout(function () {
+            PostMessage({ id: 'student', cmd: 'lastPictureTaken' }, function (data) { 
+                if (data) {
+                    status.pictureTimeStamp = data;
+
+                }
+            });
+            PollForPictureTimeStamp();
+        }, 3000);
+    }
+
+    //function UpdatePicture
     return {
         create: CreateRobotFarmer
     };
+
 })();
