89d7847f1d0df89c56d787fe98ce7dde71035737
alex-nishikawa <alex.d.nishikawa@gmail.com>
2015-09-10 13:53:30 -0700

    setup up fake unit test

---
+++ codeship/index-unit-tests.js
@@ -0,0 +1,17 @@
+﻿var should = require('should'),
+    sinon = require('sinon');
+
+describe('some unit test', function () {
+    describe('#first test', function () {
+        it('should pass fake unit test', function () {
+            //Arrange
+            var urlWithWhiteSpace = " www.foo.com ";
+            
+            //Act
+            var result = urlWithWhiteSpace.trim();
+            
+            //Assert
+            result.url.should.eql(urlWithWhiteSpace.trim());
+        });
+    });
+});
